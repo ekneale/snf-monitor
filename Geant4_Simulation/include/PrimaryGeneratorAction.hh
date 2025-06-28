681dcd5a7a27fc24d7f00bc4d62e46398a40dfb8
ekneale <eskneale1@sheffield.ac.uk>
2025-06-28 10:24:32 +0100

    Adds new IBDGen generator. Generator currently set to default positron gun.

--- Geant4_Simulation/include/PrimaryGeneratorAction.hh
+++ Geant4_Simulation/include/PrimaryGeneratorAction.hh
@@ -7,6 +7,8 @@
 #include "G4SystemOfUnits.hh"
 #include "G4ParticleGun.hh"
 
+#include "IBDGen.hh"
+
 namespace G4_BREMS
 {
 
@@ -18,7 +20,26 @@ namespace G4_BREMS
 
 		virtual void GeneratePrimaries(G4Event*);
 
-		G4ParticleGun* fParticleGun;
+	private:
+		G4ParticleGun*			particleGun;
+		IBDGen* 			ibdGen;
+//		PrimaryGeneratorMessenger*	messenger;
+
+		// Variables set by the messenger
+		G4bool 				useIBDGen=1;
+		G4String 			spectrum_database="IBDSpectra.json";
+		G4String 			spectrum_name="Flat";
+		
+	public: 
+
+		// Addition of IBD event generator
+		inline void SetIBDGenerator(G4bool choice) { useIBDGen = choice; }
+		inline G4bool isUsingIBDGen() { return useIBDGen; }
+		inline void SetIBDDatabase(G4String choice) { spectrum_database = choice; }
+		inline G4String GetIBDDatabase()  { return spectrum_database; }
+		inline void SetIBDSpectrum(G4String choice) { spectrum_name = choice; }
+		inline G4String GetIBDSpectrum()  { return spectrum_name; }
+	
 	};
 }
 
--- Geant4_Simulation/src/PrimaryGeneratorAction.cc
+++ Geant4_Simulation/src/PrimaryGeneratorAction.cc
@@ -1,4 +1,5 @@
 #include "PrimaryGeneratorAction.hh"
+
 #include "G4Event.hh"
 #include "G4ParticleTable.hh"
 #include "G4ParticleDefinition.hh"
@@ -10,45 +11,90 @@
 namespace G4_BREMS
 {
 	PrimaryGeneratorAction::PrimaryGeneratorAction() {
-		// set up particle gun
-		G4int nParticles = 1;
-		fParticleGun = new G4ParticleGun(nParticles);
-
-		// define particle properties
-		//const G4String& particleName = "opticalphoton";
-		const G4String& particleName = "e+";
-
-		G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
-		G4ParticleDefinition* particle
-			= particleTable->FindParticle(particleName);
-
-		fParticleGun->SetParticleDefinition(particle);
-
-		G4ThreeVector momentumDirection = G4ThreeVector(1, 0, 0);
-		//G4ThreeVector momentumDirection = G4ThreeVector(0, 1, 0);
-
-		//G4cout << "Particle direction: " << momentumDirection << G4endl;
-
-		fParticleGun->SetParticleMomentumDirection(momentumDirection);
-
-		G4double energy = 10.0 * MeV;
-		//G4cout << "Particle energy: " << G4BestUnit(energy, "Energy") << G4endl;
-
-		fParticleGun->SetParticleEnergy(energy);
-
+		
+		// Things to initialise here
+//		messenger = new PrimaryGeneratorMessenger(this);
+		useIBDGen = false;
+	
 	}
 
 	PrimaryGeneratorAction::~PrimaryGeneratorAction() {
-		delete fParticleGun;
+
+		if (particleGun) delete particleGun;
+		if (ibdGen) delete ibdGen;
 	}
 
 	void PrimaryGeneratorAction::GeneratePrimaries(G4Event* event)
 	{
 		
-		G4ThreeVector position = G4ThreeVector(-200.0 * mm, 0 * mm, 0 * mm);
-		
-		fParticleGun->SetParticlePosition(position);
+		if (useIBDGen){
+                	
+			// populate event from IBDGen.cc
+			
+			// Initialise the IBD generator once per simulation
+			// This will read the spectrum from the db
+			// and produce a 2D histogram of the spectrum.
+			if(!ibdGen){
+				G4cout << "PrimaryGeneratorAction::GeneratePrimaries  [INFO] Initialising IBD generator" << G4endl;
+				ibdGen = new IBDGen();
+				ibdGen->ReadSpectrumFromDB(spectrum_database,spectrum_name);
+			}
 
-		fParticleGun->GeneratePrimaryVertex(event);
+			// Create the particles
+			G4LorentzVector neutrino;
+			G4LorentzVector positron;
+			G4LorentzVector neutron;
+
+			// Generate event
+			// Fills the Lorentz vectors with the 
+			// direction and energy of the particles,
+			// with a random position in the detector
+			// and will populate the G4Event
+			G4cout << "PrimaryGeneratorAction::GeneratePrimaries  [INFO] Generating IBD interaction" << G4endl;
+			ibdGen->GenerateEvent(neutrino,positron,neutron,event);
+
+			// Populate truth information to be written out
+			// vtx
+			// dir
+			// positron energy
+			// neutron energy
+			// positron pdg
+			// neutron pdg
+			// positron momentum/direction
+			// neutron momentum/direcion
+
+		} else {
+
+			// set up particle gun by default
+			G4int nParticles = 1;
+			particleGun = new G4ParticleGun(nParticles);
+
+			// define particle properties
+			const G4String& particleName = "e+";
+
+			G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+			G4ParticleDefinition* particle
+				= particleTable->FindParticle(particleName);
+
+			particleGun->SetParticleDefinition(particle);
+
+			G4ThreeVector momentumDirection = G4ThreeVector(1, 0, 0);
+			//G4ThreeVector momentumDirection = G4ThreeVector(0, 1, 0);
+
+			//G4cout << "Particle direction: " << momentumDirection << G4endl;
+
+			particleGun->SetParticleMomentumDirection(momentumDirection);
+
+			G4double energy = 10.0 * MeV;
+			//G4cout << "Particle energy: " << G4BestUnit(energy, "Energy") << G4endl;
+
+			particleGun->SetParticleEnergy(energy);
+
+			G4ThreeVector position = G4ThreeVector(-200.0 * mm, 0 * mm, 0 * mm);
+		
+			particleGun->SetParticlePosition(position);
+
+			particleGun->GeneratePrimaryVertex(event);
+		}
 	}
 }
