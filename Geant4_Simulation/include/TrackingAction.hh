bad151e8ada2059dcf6f321bb91714bdaffe53af
ekneale <eskneale1@sheffield.ac.uk>
2026-03-04 10:34:26 +0000

    First inclusion of parent tracking for neutron and positron secondaries

---
+++ Geant4_Simulation/include/TrackingAction.hh
@@ -0,0 +1,30 @@
+#ifndef TrackingAction_h
+#define TrackingAction_h 1
+
+#include "G4UserTrackingAction.hh"
+#include "TrackInformation.hh"
+#include "globals.hh"
+
+class G4Track;
+
+/**
+ * Tracking Action class to manage track-level logic.
+ * Derived from G4UserTrackingAction.
+ */
+class TrackingAction : public G4UserTrackingAction {
+public:
+    TrackingAction();
+    virtual ~TrackingAction();
+
+    // Triggered before the track starts moving
+    virtual void PreUserTrackingAction(const G4Track* aTrack) override;
+
+    // Triggered after the track has finished its trajectory
+    virtual void PostUserTrackingAction(const G4Track* aTrack) override;
+
+private:
+
+    TrackInformation *info;
+};
+
+#endif
